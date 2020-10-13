4d70f87d56209670b97d6bfb7e03a2bab6b53353
Jordan Beck <jordanfbeck@gmail.com>
2020-10-13 16:45:16 -0600

    Test and Code for initialization of cell

---
+++ test/cell_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+    assert_equal "B4", cell.coordinate
+    assert_equal nil, cell.ship 
+  end
+end
