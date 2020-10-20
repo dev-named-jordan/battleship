c492b9596b90f12de2072879a4682401de306f9d
Robert Heath <robert.b.heathii@gmail.com>
2020-10-19 21:12:38 -0500

    Complete general game setup. Still need to debug

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,10 @@ class Turn
   def initialize
     @computer_board = Board.new
     @player_board = Board.new
+    @player_has_fired_at = []
+    @computer_has_fired_at = []
+    @computer_points = 0
+    @player_points = 0
   end
 
   def start
@@ -23,6 +27,7 @@ class Turn
   def setup_game
     computer_setup
     human_setup
+    round
   end
 
 #class Game
@@ -43,7 +48,6 @@ class Turn
       submarine_coordinates = sample_coordinates
     end
     @computer_board.place(@computer_submarine, submarine_coordinates)
-    require "pry"; binding.pry
   end
 
   def human_setup
@@ -81,6 +85,71 @@ class Turn
       submarine_setup
     end
   end
-end
 
-#start
+  def round
+    puts "=============COMPUTER BOARD============="
+    puts @computer_board.render
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+    player_shoots
+    computer_shoots
+    end_of_round_report
+    end_game
+  end
+
+  def player_shoots
+    puts "Enter the coordinate for your shot:"
+    @player_shot = gets.upcase.chomp
+    if (@computer_board.valid_coordinate?(@player_shot)) && (@player_has_fired_at.include?(@player_shot) == false)
+      ###Issue on line 104
+      @computer_board[@player_shot].fire_upon
+      @player_has_fired_at << @player_shot
+    else
+      puts "That's not a good shot. Choose again."
+      player_shoots
+    end
+  end
+
+  def computer_shoots
+    @computer_shot = @player_board.cells.sample
+    if @computer_has_fired_at.include?(@computer_shot) == false
+      @player_board[@computer_shot].fire_upon
+      @computer_has_fired_at << @computer_shot
+    else
+      computer_shoots
+    end
+  end
+
+  def end_of_round_report
+    computer_report =  @player_board.cells[@computer_shot].render
+    if computer_report == "X"
+      puts "My shot on #{@computer_shot} sunk your ship."
+    elsif computer_report == "H"
+        puts "My shot on #{@computer_shot} was a hit."
+        @computer_points += 1
+    else
+      puts "My shot on #{@computer_shot} was a miss."
+    end
+    player_report =  @computer_board.cells[@player_shot].render
+    if player_report == "X"
+      puts "My shot on #{@player_shot} sunk your ship."
+    elsif player_report == "H"
+        puts "My shot on #{@player_shot} was a hit."
+        @player_points += 1
+    else
+      puts "My shot on #{@player_shot} was a miss."
+    end
+  end
+
+  def end_game
+    if @player_points == 5 && @computer_points == 5
+      puts "It's tie!"
+    elsif @player_points == 5
+      puts "You won!"
+    elsif @player_points == 5
+      puts "I won!"
+    else
+      round
+    end
+  end
+end
